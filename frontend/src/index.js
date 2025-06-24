cacab2baa981f87cf4d6045cd24b8b9bdb4e60b8
Hammad8ahmad <hammad.27a@gmail.com>
2025-06-24 12:43:37 +0500

    fixed mobile nav bug

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// import './index.css';
+import "./index_commented.css"
 import App from './App';
 import { WorkoutsContextProvider } from './context/WorkoutsContext';
 import { ThemeContextProvider } from './context/ThemeContext';
